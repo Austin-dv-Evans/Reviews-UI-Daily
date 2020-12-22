fa76cd3549d22bf3521faa80dc2a9a2fcd774776
Austin-Vincelli-Evans <unity7591@gmail.com>
2020-12-22 10:51:53 -0700

    reviews UI

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Renee Vincelli",
+    job: "web developer",
+    image:
+      "https://scontent.fapa1-1.fna.fbcdn.net/v/t1.0-9/122458182_10157392099511283_5102256130180240907_n.jpg?_nc_cat=103&ccb=2&_nc_sid=09cbfe&_nc_ohc=5GfYiR2jM_UAX8Idm5P&_nc_ht=scontent.fapa1-1.fna&oh=f225bdb240fe6a34c9657af3ef0273b7&oe=6006E609",
+    text:
+      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: "Luna Rae",
+    job: "web designer",
+    image:
+      "https://scontent.fapa1-1.fna.fbcdn.net/v/t1.0-9/131307917_2101884839945919_7694038603738872618_o.jpg?_nc_cat=106&ccb=2&_nc_sid=730e14&_nc_ohc=iIwLhKziN8oAX-H5MHD&_nc_ht=scontent.fapa1-1.fna&oh=adc9dfa426aadcf18b3552476c1cdee9&oe=6005C223",
+    text:
+      "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+  },
+  {
+    id: 3,
+    name: "Austin David",
+    job: "intern",
+    image:
+      "https://scontent.fapa1-2.fna.fbcdn.net/v/t1.0-9/121619176_2041913392609731_5869671214843247918_o.jpg?_nc_cat=100&ccb=2&_nc_sid=09cbfe&_nc_ohc=jF4eoZxYneMAX_37vPF&_nc_ht=scontent.fapa1-2.fna&oh=de5f72edf089645b2792321671a56f5d&oe=60074263",
+    text:
+      "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
+  {
+    id: 4,
+    name: "Dominique McCulley",
+    job: "the boss",
+    image:
+      "https://scontent.fapa1-2.fna.fbcdn.net/v/t1.0-9/95660306_10163405054715304_522552951324016640_n.jpg?_nc_cat=102&ccb=2&_nc_sid=09cbfe&_nc_ohc=IkbfzQUUn8sAX-9Tzmh&_nc_ht=scontent.fapa1-2.fna&oh=8300c4650483cd08276ffddf3701d876&oe=6007751A",
+    text:
+      "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+  },
+];
+
+export default reviews;
